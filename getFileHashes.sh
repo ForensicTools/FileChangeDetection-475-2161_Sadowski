952900b79fea14cdd4f599bd8372fea0b0378a71
Jackson <veracus@users.noreply.github.com>
2016-10-21 23:19:32 -0400

    Update getFileHashes.sh

--- getFileHashes.sh
+++ getFileHashes.sh
@@ -3,7 +3,6 @@
 
 #SET VARIABLES HERE
 DIRECT='/root/check/'
-HASHFILE='/root/hashes.txt'
 
 #sha256 hash files in directory
 for file in `find $DIRECT -iname "*"`; do sha256sum $file 2> /dev/null 1; done
