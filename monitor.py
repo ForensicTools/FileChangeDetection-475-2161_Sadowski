9fa280e34664d2da743561faf193a8ea3d6c2538
veracus <jps5374@rit.edu>
2016-11-18 21:32:14 -0500

    Adding additional logic for monitoring directories

--- monitor.py
+++ monitor.py
@@ -2,11 +2,12 @@
 #Jackson Sadowski
 
 from subprocess import Popen, PIPE
+from time import sleep
 
 # Getting hashes of files
 def getFileHashes(directory):
 
-   (stdout, stderr) = Popen(["bash","/root/getFileHashes.sh"], stdout=PIPE).communicate()
+   (stdout, stderr) = Popen(["bash","./getFileHashes.sh"], stdout=PIPE).communicate()
 
    fileHashes = {}
 
@@ -24,13 +25,30 @@ def getFileHashes(directory):
 # Main
 def main():
 
-   directory = "/root/check/"
+   directory = "./check/"
 
    # Getting files
-   fileHashes = getFileHashes(directory)
+   oldHashes = getFileHashes(directory)
 
-   print "=== File hashes ==="
-   for k,v in fileHashes.items():
+   print "=== Initial File hashes ==="
+   for k,v in oldHashes.items():
       print k, v
+   print "==========================="
+   print ""
+   print "Monitoring directory.."
+   print ""
+
+
+   while True:
+      newFileHashes = getFileHashes(directory)
+      for k,v in newFileHashes.items():
+         if k not in oldHashes.keys():
+            print "File Updated: " + v
+            print "Hash: " + k
+            
+      sleep(2)
+      oldHashes = newFileHashes
+
 
 main()
+
